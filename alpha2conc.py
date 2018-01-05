1684423492dcb098f604a692cd3734109c09eb78
Alex Ganose <alexganose@googlemail.com>
2018-01-05 14:59:30 +0000

    Update to remove pvpy dependancy

--- alpha2conc.py
+++ alpha2conc.py
@@ -1,52 +1,54 @@
 import numpy as np
+from scipy import interpolate, constants
 
-from scipy import interpolate
-from scipy.constants import physical_constants
-
-from pvpy.PowerSpectrum import PhotonSpectrum
-
-
-hv = (physical_constants['Planck constant in eV s'][0] *
-      physical_constants['speed of light in vacuum'][0])
+h = constants.physical_constants['Planck constant in eV s'][0]
+c = constants.physical_constants['speed of light in vacuum'][0]
 
 
-def alpha2conc(energies, absorption, wavelength_limits=(280, 2000),
-               thickness=2e-5, delta_wavelength=5, delta_thickness=1e-7,
-               carrier_lifetime=1e-5):
+def alpha2conc(energies, absorption, thickness, tau, energy_limits=None,
+               de=0.001, dt=1e-7):
     """Calculate the carrier concentration of a material based on its absorption
 
     Args:
-        energies (np.array): Photon energies in eV
-        absorption (np.array): Optical absorption in cm^-1
-        wavelength_limits (tuple): Integration limits for the photon wavelengths
-            as (min, max), in cm^-1.
-        thickness (float): Thickness of material in cm
-        delta_wavelength (float): Wavelength integration step size
-        delta_thickness (float): Thickness integraton step size
-        carrier_lifetime (float): Carrier lifetime of excited carriers
+        energies (np.array): Photon energies in eV.
+        absorption (np.array): Optical absorption in cm^-1.
+        thickness (float): Thickness of material in cm.
+        tau (float): Carrier lifetime of excited carriers in s.
+        energy_limits (tuple): Integration limits for the photon energy
+            as (min, max), in eV. Default is None (all energies).
+        de (float): Energy integration step size in eV. Default 0.05 eV.
+        dt (float): Thickness integraton step size in cm. Default 2 nm.
 
     Returns:
         Carrier concentration in cm^-3.
     """
-    ps = PhotonSpectrum()
-    flux_spectrum = ps.get_spectrum()
+    am_eners, am_spec = np.loadtxt('AM15G.csv', unpack=True, delimiter=',')
 
-    # convert spectrum from m^-2 s^-1 nm to cm^-2 s^-1 nm and interpolate
-    flux = interpolate.interp1d(flux_spectrum[0, :], flux_spectrum[1, :] * 1e-4)
+    # convert to photon flux -- from Watts/(meter**2 nm) to #/(s meter**2 nm)
+    flux = am_spec * am_eners * 1e-9 / (constants.c * constants.h)
 
-    # convert energies to cm^-1 and interpolate absorption
-    alpha = interpolate.interp1d(hv * 1e9 / energies, absorption)
+    # convert photon flux and energies from nm to eV
+    flux = (flux * am_eners**2 * 1e-9) / (h * c)
+    am_eners = (h * c) / (am_eners * 1e-9)
+
+    # convert flux from m^-2 s^-1 eV^-1 to cm^-2 s^-1 eV^-1 and interpolate
+    flux = interpolate.interp1d(am_eners, flux * 1e-4)
+
+    # interpolate absorption
+    alpha = interpolate.interp1d(energies, absorption)
 
     # widths and wavelengths over which to integrate
-    widths = np.arange(0, thickness, delta_thickness)
-    wavelengths = np.arange(wavelength_limits[0], wavelength_limits[1],
-                            delta_wavelength)
+    widths = np.arange(0, thickness, dt)
+
+    if not energy_limits:
+        energy_limits = [max([min(energies), min(am_eners)]),
+                         min([max(energies), max(am_eners)])]
+    int_eners = np.arange(energy_limits[0], energy_limits[1], de)
 
     gen_total = 0
     for w in widths:
-        exps = np.exp(-alpha(wavelengths) * w)
-        gen_total += (np.sum(alpha(wavelengths) * flux(wavelengths) * exps) *
-                      delta_wavelength)
-    gen_total = gen_total * delta_thickness
+        exps = np.exp(-alpha(int_eners) * w)
+        gen_total += np.sum(alpha(int_eners) * flux(int_eners) * exps) * de
+    gen_total = gen_total * dt
 
-    return(gen_total * carrier_lifetime)
+    return(gen_total * tau / thickness)
